9a7f424c2eb70d1cfceae1d2a88c08d5fe8f8680
htsmile <83398139+htsimle@users.noreply.github.com>
2022-08-04 13:44:13 +0500

    Update file.rb

--- file.rb
+++ file.rb
@@ -3,16 +3,19 @@ require 'timeout'
 quiz = CSV.read("problems.csv")
 wrongAns = 0
 rightAns = 0
-for i in 0..11  
-   puts "Q#{i+1}" + ": -> What is " + quiz[i][0] +" ?"
-   ans = gets.chomp.to_i 
-   
-   if ans == quiz[i][1].chomp.to_i
-      rightAns = rightAns+1
-   else
-      wrongAns = wrongAns+1
+
+status = Timeout::timeout(5) {
+   for i in 0..11  
+      puts "Q#{i+1}" + ": -> What is " + quiz[i][0] +" ?"
+      ans = gets.chomp.to_i 
+      
+      if ans == quiz[i][1].chomp.to_i
+         rightAns = rightAns+1
+      else
+         wrongAns = wrongAns+1
+      end
    end
-end
+}
 puts "You Provide #{rightAns} right and #{wrongAns} wrong answers."
 
 puts "Press 1 if you want to change the file name else 0"

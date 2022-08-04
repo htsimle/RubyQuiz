601c33b65636f183c5d1d36c91498c5a6bfd8453
htsmile <83398139+htsimle@users.noreply.github.com>
2022-08-04 13:27:44 +0500

    Update file.rb

--- file.rb
+++ file.rb
@@ -1,14 +1,24 @@
 require 'csv'
-quiz = CSV.read("problems.csv")
+require 'timeout'
+quiz = File.read("file.rb")
 wrongAns = 0
 rightAns = 0
 for i in 0..11  
-     puts "Q#{i+1}" + ": -> What is " + quiz[i][0] +" ?"
-     ans = gets.chomp.to_i 
-     if ans == quiz[i][1].chomp.to_i
-        rightAns = rightAns+1
-     else
-        wrongAns = wrongAns+1
-     end
+   puts "Q#{i+1}" + ": -> What is " + quiz[i][0] +" ?"
+   ans = gets.chomp.to_i 
+   
+   if ans == quiz[i][1].chomp.to_i
+      rightAns = rightAns+1
+   else
+      wrongAns = wrongAns+1
+   end
 end
 puts "You Provide #{rightAns} right and #{wrongAns} wrong answers."
+
+puts "Press 1 if you want to change the file name else 0"
+flag = gets.chomp.to_i
+if flag == 1
+   puts "Now, Enter the New filename: "
+   fileName= gets
+   File.rename("problem.csv", fileName + ".csv")
+end
